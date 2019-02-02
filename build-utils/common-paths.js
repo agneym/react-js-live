dd97da6fe78b2ad4103e95e221e3cb1a659155b4
Boy with Silver Wings <agney@outlook.in>
2019-02-02 18:05:37 +0530

    add common config

---
+++ build-utils/common-paths.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+module.exports = {
+  root: path.resolve(__dirname, ".."),
+  outputPath: path.resolve(__dirname, "../", "build"),
+  appSrc: path.resolve(__dirname, "../", "src"),
+};
---
+++ build-utils/webpack.common.js
@@ -0,0 +1,37 @@
+const commonPaths = require("./common-paths");
+const webpack = require("webpack");
+
+const config = {
+  output: {
+    filename: "[hash].bundle.js",
+    path: commonPaths.outputPath,
+    publicPath: "/"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|gif|svg|eot|ttf|woff|woff2)$/i,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 1000
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+  ]
+};
+
+module.exports = config;
